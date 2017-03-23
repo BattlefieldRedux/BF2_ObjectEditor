d2fa8bd36762dc1067f459ddbba43c5f96b53906
BenQuickDeNN <2413269761@qq.com>
2017-03-23 16:44:42 +0800

    initiation

---
+++ BF2_ObjectEditor/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BF2_ObjectEditor
+{
+    public partial class Form_MainMenu : Form
+    {
+        public Form_MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
